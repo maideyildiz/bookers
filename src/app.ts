f632704a2ad3e91e50e71fb0ddd978cdf1147200
maideyildiz <maideyildizz@gmail.com>
2022-10-02 16:10:32 +0300

    edit

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
-//const morgan = require("morgan");
 const bookRouter = require('./routes/bookRoutes');
+//const userRouter = require("./routes/userRoutes");
 //const groupRouter = require("./routes/groupRoutes");
 
 const app = express();
@@ -8,6 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/v1/books', bookRouter);
+//app.use('/api/v1/users', userRouter);
 //app.use("/api/v1/group", groupRouter);
 
 module.exports = app;
