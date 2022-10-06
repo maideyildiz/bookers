667959d6fba24aa32e6cd5da652b292318161fdc
maideyildiz <maideyildizz@gmail.com>
2022-10-06 22:04:31 +0300

    rate limiting

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import rateLimit from 'express-rate-limit';
 const bookRouter = require('./routes/bookRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./middleware/appError');
@@ -16,6 +17,12 @@ app.all('*', (req, res, next) => {
     next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many request from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
 app.use(globalErrorHandler);
 
 module.exports = app;
