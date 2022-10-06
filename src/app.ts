378e899a2b22bb3731a07d85d53368399ee8c5c1
maideyildiz <maideyildizz@gmail.com>
2022-10-06 22:16:40 +0300

    data sanitization

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import rateLimit from 'express-rate-limit';
 import helmet from 'helmet';
+import mongoSantize from 'express-mongo-sanitize';
+import xss from 'xss-clean';
 const bookRouter = require('./routes/bookRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./middleware/appError');
@@ -10,6 +12,11 @@ const globalErrorHandler = require('./controllers/errorController');
 const app = express();
 app.use(helmet());
 app.use(express.json({ limit: '10kb' }));
+
+//Data sanitization agains NoSql query injection
+app.use(mongoSantize());
+//Data sanitization XSS
+app.use(xss());
 
 app.use('/api/v1/books', bookRouter);
 app.use('/api/v1/users', userRouter);
